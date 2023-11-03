c952a5d1890d23448d350954d4a178ba6a5daabe
Sebastian Gebski <sebastian@gebski.net>
2023-11-03 19:09:26 +0100

    Switch from Gradio to Streamlit

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-import gradio as gr
+import json
+import streamlit as st
 
 
-def translate(input):
+def read_input(file_name="./input.json"):
+    with open(file_name, "r") as file:
+        data = json.load(file)
+    return data
+
+
+def pick_word(json_data):
+    words = json_data["words"]
+    import random
+    word = random.choice(words)
+    st.session_state['word'] = word
+
+
+def compare_words(word1, word2):
+    # remove whitespace from both words
+    word1 = word1.strip()
+    word2 = word2.strip()
+    # compare two words, case insensitive, return True if equal
+    return word1.lower() == word2.lower()
+
+def translate(to_be_translated):
     import requests
 
     API_TOKEN = "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"
@@ -14,12 +35,32 @@ def translate(input):
         return data[0]["translation_text"]
 
     output = query({
-        "inputs": input,
+        "inputs": to_be_translated,
     })
     return output
 
 
-app = gr.Interface(fn=translate, inputs="text", outputs="text", title="Help a Skullgirl prepare for English lessons!")
+def render_ui():
+    st.session_state['points'] = 0
+    st.title("Help a Skullgirl prepare for English lessons!")
+    st.write("Randomly chosen word: " + st.session_state['word'])
+    input_text = st.text_input("Translate this word to English", key="input")
+    if st.button("Compare"):
+        st.write("Proper translation: " + translate(st.session_state['word']))
+        if compare_words(st.session_state['word'], input_text):
+            st.write("Correct!")
+            st.session_state['points'] += 1
+            if st.button("Next word"):
+                pick_word(read_file)
+        else:
+            st.write("Wrong!")
+            st.session_state['points'] = 0
+            if st.button("Next word"):
+                pick_word(read_file)
+    st.write("Points: " + str(st.session_state['points']))
+
 
 if __name__ == '__main__':
-    app.launch(show_api=False)
+    read_file = read_input()
+    pick_word(read_file)
+    render_ui()
